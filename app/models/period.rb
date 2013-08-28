eaff42ad5b06a5da29e93e6757e7a4efc562fec4
dbrodhead <daniel.brodhead@outlook.com>
2013-08-28 01:58:35 +0000

    Updated Period default scope order

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,6 +1,6 @@
 class Period < ActiveRecord::Base
   attr_accessible :etime, :name, :number, :stime, :pgroup_id
-  default_scope order('name ASC')
+  default_scope order('pgroup_id ASC')
   
   has_many :tsessions, :dependent => :destroy
   belongs_to :pgroup
